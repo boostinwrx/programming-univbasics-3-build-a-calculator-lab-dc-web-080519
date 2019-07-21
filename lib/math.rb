c126260bcb0d04f74fc99343878882c5d631dad3
ANTONIO J ABRAHAM <tony.calablava@gmail.com>
2019-07-21 04:58:19 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -2,8 +2,10 @@ def addition(num1, num2)
   sum = num1 + num2
   return sum
 end
-def subtraction(num1, num2)
 
+def subtraction(num1, num2)
+  sum = num2 - num1
+  return sum
 end
 
 def division(num1, num2)
